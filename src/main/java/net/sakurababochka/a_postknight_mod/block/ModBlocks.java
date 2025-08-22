f3f472703f95edc4af327d9a3fe54260fee7f493
SakuraBabochka <sakurababochka@gmail.com>
2025-08-22 14:40:33 +0100

    Stairs, slabs, fences, and fence gates

--- src/main/java/net/sakurababochka/a_postknight_mod/block/ModBlocks.java
+++ src/main/java/net/sakurababochka/a_postknight_mod/block/ModBlocks.java
@@ -6,8 +6,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.BlockGetter;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.material.Material;
@@ -29,49 +28,37 @@ public class ModBlocks {
     //Metal
     public static final RegistryObject<Block> BRONZE_ORE = registerBlock("bronze_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> DEEPSLATE_BRONZE_ORE = registerBlock("deepslate_bronze_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> RAW_BRONZE_BLOCK = registerBlock("raw_bronze_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> BRONZE_BLOCK = registerBlock("bronze_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
 
     public static final RegistryObject<Block> SILVER_ORE = registerBlock("silver_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> DEEPSLATE_SILVER_ORE = registerBlock("deepslate_silver_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> RAW_SILVER_BLOCK = registerBlock("raw_silver_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> SILVER_BLOCK = registerBlock("silver_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
 
     public static final RegistryObject<Block> PLATINUM_ORE = registerBlock("platinum_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(3f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> DEEPSLATE_PLATINUM_ORE = registerBlock("deepslate_platinum_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> RAW_PLATINUM_BLOCK = registerBlock("raw_platinum_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> PLATINUM_BLOCK = registerBlock("platinum_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
 
     public static final RegistryObject<Block> DRAGONITE_ORE = registerBlock("dragonite_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL).strength(4f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> DEEPSLATE_DRAGONITE_ORE = registerBlock("deepslate_dragonite_ore",
             () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL).strength(8f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> RAW_DRAGONITE_BLOCK = registerBlock("raw_dragonite_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL).strength(12f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
-
     public static final RegistryObject<Block> DRAGONITE_BLOCK = registerBlock("dragonite_block",
             () -> new Block(BlockBehaviour.Properties.of(Material.HEAVY_METAL).strength(12f).requiresCorrectToolForDrops()), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
 
@@ -99,6 +86,14 @@ public class ModBlocks {
             return 5;
         }
             }, ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> WALNUT_STAIRS = registerBlock("walnut_stairs",
+            () -> new StairBlock(() -> ModBlocks.WALNUT_PLANKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.OAK_STAIRS)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> WALNUT_SLAB = registerBlock("walnut_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.OAK_SLAB)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> WALNUT_FENCE = registerBlock("walnut_fence",
+            () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> WALNUT_FENCE_GATE = registerBlock("walnut_fence_gate",
+            () -> new FenceGateBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE_GATE)),ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
 
     public static final RegistryObject<Block> MAPLE_LOG = registerBlock("maple_log",
             () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
@@ -123,6 +118,14 @@ public class ModBlocks {
                     return 5;
                 }
             }, ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> MAPLE_STAIRS = registerBlock("maple_stairs",
+            () -> new StairBlock(() -> ModBlocks.WALNUT_PLANKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.OAK_STAIRS)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> MAPLE_SLAB = registerBlock("maple_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.OAK_SLAB)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> MAPLE_FENCE = registerBlock("maple_fence",
+            () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> MAPLE_FENCE_GATE = registerBlock("maple_fence_gate",
+            () -> new FenceGateBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE_GATE)),ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
 
     public static final RegistryObject<Block> FROZEN_FIR_LOG = registerBlock("frozen_fir_log",
             () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
@@ -147,6 +150,14 @@ public class ModBlocks {
                     return 5;
                 }
             }, ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> FROZEN_FIR_STAIRS = registerBlock("frozen_fir_stairs",
+            () -> new StairBlock(() -> ModBlocks.WALNUT_PLANKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.OAK_STAIRS)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> FROZEN_FIR_SLAB = registerBlock("frozen_fir_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.OAK_SLAB)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> FROZEN_FIR_FENCE = registerBlock("frozen_fir_fence",
+            () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> FROZEN_FIR_FENCE_GATE = registerBlock("frozen_fir_fence_gate",
+            () -> new FenceGateBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE_GATE)),ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
 
     public static final RegistryObject<Block> CYPRESS_LOG = registerBlock("cypress_log",
             () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
@@ -171,6 +182,14 @@ public class ModBlocks {
                     return 5;
                 }
             }, ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> CYPRESS_STAIRS = registerBlock("cypress_stairs",
+            () -> new StairBlock(() -> ModBlocks.WALNUT_PLANKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.OAK_STAIRS)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> CYPRESS_SLAB = registerBlock("cypress_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.OAK_SLAB)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> CYPRESS_FENCE = registerBlock("cypress_fence",
+            () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> CYPRESS_FENCE_GATE = registerBlock("cypress_fence_gate",
+            () -> new FenceGateBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE_GATE)),ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
 
     public static final RegistryObject<Block> CORAL_LOG = registerBlock("coral_log",
             () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.copy(Blocks.OAK_LOG)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
@@ -195,6 +214,14 @@ public class ModBlocks {
                     return 5;
                 }
             }, ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> CORAL_STAIRS = registerBlock("coral_stairs",
+            () -> new StairBlock(() -> ModBlocks.WALNUT_PLANKS.get().defaultBlockState(), BlockBehaviour.Properties.copy(Blocks.OAK_STAIRS)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> CORAL_SLAB = registerBlock("coral_slab",
+            () -> new SlabBlock(BlockBehaviour.Properties.copy(Blocks.OAK_SLAB)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> CORAL_FENCE = registerBlock("coral_fence",
+            () -> new FenceBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE)), ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
+    public static final RegistryObject<Block> CORAL_FENCE_GATE = registerBlock("coral_fence_gate",
+            () -> new FenceGateBlock(BlockBehaviour.Properties.copy(Blocks.OAK_FENCE_GATE)),ModCreativeModeTabs.POSTKNIGHT_RESOURCE_TAB);
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
